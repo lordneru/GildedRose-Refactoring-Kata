fb98794c05b7e8f92d865fe8461be3cc695f94cd
Javier Almansa <j.almansa@wata.es>
2018-05-16 15:26:11 +0200

    init refactor kata

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,23 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20),
+			new Item("Aged Brie", 2, 0),
+			new Item("Elixir of the Mongoose", 5, 7),
+			new Item("Sulfuras", 0, 80),
+			new Item("Sulfuras", -1, 80),
+			new Item("Backstage", 15, 20),
+			new Item("Backstage", 10, 49),
+			new Item("Backstage", 5, 49),
+			new Item("Conjured Mana Cake", 3, 6) };
+	
+	GildedRose app = new GildedRose(items);
+	
+	@Test
+	public void decreaseQualityNormalItems() {
+		app.updateQuality();
+		assertEquals(19, app.items[0].quality);
+		assertEquals(10, app.items[0].sellIn);
+	}
 
 }
